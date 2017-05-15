f0abc3ea66d84b221a3c352ce091037b908575c6
byelipk <byelipk@gmail.com>
2017-05-15 11:53:52 -0400

    Implement adding node to bst

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -25,6 +25,84 @@
 //   return -1
 // end
 
+class Node {
+  constructor(value, left=null, right=null) {
+    this.value = value;
+    this.left  = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  
+  constructor(data) {
+    this.root   = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    // No root node, create root
+    if (this.root === null) {
+      this.root = new Node(value);
+      this.length += 1;
+      return;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (current.value > value) {
+        // Go left!
+
+        if (current.left) {
+          current = current.left;
+        }
+        else {
+          current.left = new Node(value, null, current);
+          break;
+        }
+      }
+      else {
+        // Go right!
+
+        if (current.right) {
+          current = current.right;
+        }
+        else {
+          current.right = new Node(value, current, null);
+          break;
+        }
+      }
+    }
+
+    this.length += 1;
+  }
+
+  search(value) {
+    let left  = 0;                 // left side iterator
+    let right = this.length - 1;   // right side iterator
+    let current = this.root;
+
+    while (left <= right) {
+      let index = Math.floor((left + right) / 2);
+
+      if (value === current.value) {       // index found
+        return index;
+      }
+      else if (value < current.value) {    // decrement
+        right = index - 1;
+        current = current.left;
+      }
+      else {                            // increment
+        left  = index + 1;
+        current = current.right;
+      }
+    }
+
+    return false;
+  }
+}
+
 class BST {
 
   constructor(data) {
@@ -37,6 +115,7 @@ class BST {
 
     while (left <= right) {
       let m = Math.floor((left + right) / 2);
+      console.log(m)
 
       if      (value === this.data[m]) { return m; }       // index found
       else if (value < this.data[m])   { right = m - 1; }  // decrement
@@ -50,8 +129,16 @@ class BST {
 
 // NOTE
 // Binary search trees can only process sorted data.
-const data = [1,2,3,4,5];
+// const data = [1,2,3,4,5];
+// const tree = new BST(data);
+// console.log(tree.search(1));
 
-const tree = new BST(data);
+const tree = new Tree();
 
-console.log(tree.search(5));
+tree.add(1);
+tree.add(2);
+tree.add(3);
+tree.add(4);
+
+console.log(tree);
+// console.log(tree.search(1));
